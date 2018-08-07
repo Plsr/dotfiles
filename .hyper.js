104adbbb385e06ae1bbe95a8abc2301c3f0df97a
Plsr <christianpoplawski@gmail.com>
2018-08-07 14:03:58 +0200

    [hyper] Add hypercwd
    When opening new splits or tabs, they're opened in the current working
    directory instead of $HOME.

--- .hyper.js
+++ .hyper.js
@@ -132,7 +132,8 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyper-one-dark' 
+    'hyper-one-dark',
+    'hypercwd'
   ],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
