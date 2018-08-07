736f05b0758a90fecb1441c109573947fe8683d5
Plsr <christianpoplawski@gmail.com>
2018-08-07 14:05:07 +0200

    [shell] Add alias for git checkout -b

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -12,6 +12,7 @@ alias gc="git commit"
 alias gp="git push"
 alias gb="git branch"
 alias gco="git checkout"
+alias gcob="git checkout -b"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset
 %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
