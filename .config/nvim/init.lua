ad4a356f658ddb8b94d6192d6178173882a7018a
Christian Poplawski <cpoplaws@cisco.com>
2022-10-26 10:00:33 +0200

    Add missing init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+require("chrisjarling.plugins-setup")
+require("chrisjarling.core.options")
+require("chrisjarling.core.colorscheme")
+require("chrisjarling.core.keymaps")
+
+require("chrisjarling.plugins.nvim-tree")
+require("chrisjarling.plugins.lualine")
+require("chrisjarling.plugins.telescope")
+require("chrisjarling.plugins.coc")
