8dae22aec3470e89b48782cd34d5d32a24a410f6
Plsr <christian@gigs.com>
2023-05-08 19:10:35 +0200

    Add coc

---
+++ .config/nvim/after/plugin/coc.lua
@@ -0,0 +1,19 @@
+-- Some servers have issues with backup files
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+vim.opt.updatetime = 300
+
+vim.opt.signcolumn = "yes"
+
+local keyset = vim.keymap.set
+
+function _G.check_back_space()
+  local col = vim.fn.col('.') - 1
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
