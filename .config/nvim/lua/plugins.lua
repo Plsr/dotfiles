b516232aa6b46f3e915278126241c6bd7fce70bf
Plsr <christian@gigs.com>
2023-05-19 05:46:51 +0200

    Format plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,15 +7,14 @@ vim.cmd([[
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use { 'neoclide/coc.nvim', branch = 'release'}
+  use { 'neoclide/coc.nvim', branch = 'release' }
   use "savq/melange-nvim"
-  use('nvim-treesitter/nvim-treesitter', {_run = ':TSUpdate' })
+  use('nvim-treesitter/nvim-treesitter', { _run = ':TSUpdate' })
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'github/copilot.vim'
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
 end)
-
