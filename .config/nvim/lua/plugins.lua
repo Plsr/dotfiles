695f6aefe5c2c0f816c073944064a3ce42104cfe
Plsr <christianpoplawski@gmail.com>
2024-04-02 01:46:58 +0200

    Remove pinned telescope tag

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
   use { 'neoclide/coc.nvim', branch = 'release' }
   use('nvim-treesitter/nvim-treesitter', { _run = ':TSUpdate' })
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use 'github/copilot.vim'
