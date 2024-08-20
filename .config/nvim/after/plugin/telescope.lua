f8ca569ce26c69b03662dd93dc1d5db11c862a4f
Plsr <christian@gigs.com>
2024-08-20 08:16:08 +0200

    Truncate file paths in telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,11 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup({
+  defaults = {
+    path_display = { "truncate" }
+  }
+})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
