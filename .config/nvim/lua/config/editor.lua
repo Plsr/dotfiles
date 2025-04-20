00c16383f578efe63062510f59bc31009f50fb00
Plsr <christianpoplawski@gmail.com>
2025-04-21 00:19:54 +0200

    Add new nvim config & setup nvim-cmp

---
+++ .config/nvim/lua/config/editor.lua
@@ -0,0 +1,5 @@
+vim.o.tabstop = 2 -- Insert 2 spaces for a tab
+vim.o.shiftwidth = 2 -- Insert 2 spaces for indentation
+vim.o.expandtab = true -- Convert tabs to spaces (sorry richard)
+vim.wo.number = true -- show line numbers
+
---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,42 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer", -- source for text in buffer
+    "hrsh7th/cmp-path", -- source for file system paths
+    "L3MON4D3/LuaSnip", -- snippet engine
+    "saadparwaiz1/cmp_luasnip", -- completions for snippets
+    "rafamadriz/friendly-snippets", -- useful snippets
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    -- loads vscode style snippets from intalled plugins
+    -- e.g. from friendly-snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect"
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+      sources = cmp.config.sources({
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
