387bd524ccfe1a6da874518a75af432ca8a12fbb
Plsr <christian@gigs.com>
2025-04-22 08:41:51 +0200

    Add vague colorscheme

--- .config/nvim/lua/config/editor.lua
+++ .config/nvim/lua/config/editor.lua
@@ -1,5 +1,6 @@
-vim.o.tabstop = 2 -- Insert 2 spaces for a tab
-vim.o.shiftwidth = 2 -- Insert 2 spaces for indentation
+vim.o.tabstop = 2      -- Insert 2 spaces for a tab
+vim.o.shiftwidth = 2   -- Insert 2 spaces for indentation
 vim.o.expandtab = true -- Convert tabs to spaces (sorry richard)
-vim.wo.number = true -- show line numbers
+vim.wo.number = true   -- show line numbers
 
+vim.cmd [[colorscheme vague]]
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  "vague2k/vague.nvim",
+  config = function()
+    -- NOTE: you do not need to call setup if you don't want to.
+    require("vague").setup({
+      -- optional configuration here
+    })
+  end
+}
