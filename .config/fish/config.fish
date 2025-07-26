310aa6787ed76f8ae94d5fa5c573c4d49b6f6fe6
Chris Jarling <christianpoplawski@gmail.com>
2025-07-26 08:06:22 +0200

    Initial Commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# pnpm
+set -gx PNPM_HOME "/Users/cj/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
