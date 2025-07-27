44f1c9aaacdccbb13acde19d83971b1e522e1c4b
Chris Jarling <christianpoplawski@gmail.com>
2025-07-27 13:59:34 +0200

    Add back some aliases

---
+++ .config/fish/alias.fish
@@ -0,0 +1,12 @@
+# Applications
+alias vim="nvim"
+
+# Git
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
+alias gst="git status"
+alias gco="git checkout"
+alias gp="git push"
+alias gpu="git push -u origin HEAD"
+alias gf="git pull"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+source ~/dotfiles/.config/fish/alias.fish
+
 # pnpm
 set -gx PNPM_HOME "/Users/cj/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
